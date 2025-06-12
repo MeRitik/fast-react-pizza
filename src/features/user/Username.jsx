cbf7540624f67dc78671631c890612b7498f98ea
Ritik Ranjan <ritikranjan999@gmail.com>
2025-06-12 19:18:52 +0530

    Enhance UI components with improved styling and layout adjustments

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Username() {
     return (
-        <p className='text-sm font-semibold'>Ritik</p>
+        <p className='text-sm font-semibold hidden'>Ritik</p>
     )
 }
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,13 +1,16 @@
+import CreateUser from '../features/user/CreateUser';
+
 function Home() {
   return (
-    <div>
-      <h1 className="font-semibold text-center text-xl">
+    <div className='sm:my-16 my-10 text-center'>
+      <h1 className="font-semibold text-xl mb-8">
         The best pizza.
         <br />
         <span className="text-yellow-500">
           Straight ot of the oven, straight to you.
         </span>
       </h1>
+      <CreateUser />
     </div>
   );
 }
