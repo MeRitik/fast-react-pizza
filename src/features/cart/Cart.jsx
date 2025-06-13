db6958d9827890ca9341ffecdb5aeaa4e2eaa576
Ritik Ranjan <ritikranjan999@gmail.com>
2025-06-13 11:57:54 +0530

    feat: enhance cart and order components with Tailwind CSS styling and improved layout

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
+import CartItem from './CartItem';
 
 const fakeCart = [
   {
@@ -31,14 +32,18 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div>
+    <div className='px-4 py-3'>
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className='mt-7 text-xl font-semibold'>Your cart, %NAME%</h2>
 
-      <div>
+      <ul className='divide-y divide-stone-500 border-b mt-3'>
+        {cart.map(item => <CartItem item={item} key={item.key} />)}
+      </ul>
+
+      <div className='mt-6 space-x-2'>
         <Button type='primary' to="/order/new">Order pizzas</Button>
-        <button>Clear cart</button>
+        <Button type='secondary'>Clear cart</Button>
       </div>
     </div>
   );
